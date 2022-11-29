119b71911e11a1ed15c689a83d2030f4c73f9387
Frederic <zetachow@163.com>
2022-11-29 13:33:21 +0800

    默认完整地址，添加Open浏览器

--- main.go
+++ main.go
@@ -9,10 +9,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
-	var addr = flag.String("addr", ":8088", "127.0.0.1:8088 or :8088")
+	var addr = flag.String("addr", "127.0.0.1:8088", "127.0.0.1:8088 or :8088")
 	var repo = flag.String("repo", "./repo", "repo path, default ./repo") //可自定义repo路径
 
 	flag.Parse()
@@ -31,9 +33,27 @@ func main() {
 	localstore.InitDB(*repo)
 	ipfsnode.Start(ctx, *repo)
 
+	go open(fmt.Sprintf("http://%s/", *addr))
+
 	//启动web模块
 	if err := web.Start(*addr); err != nil {
 		fmt.Println(err.Error())
 	}
 
+}
+func open(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start"}
+	case "darwin":
+		cmd = "open"
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+	}
+	args = append(args, url)
+	return exec.Command(cmd, args...).Start()
 }
