420dd32769f1317469e6aa43e8b86198c339d998
Frederic <zetachow@163.com>
2023-01-29 00:42:20 +0800

    本库改为orbitdb实例工具

--- database/database.go
+++ database/database.go
@@ -2,102 +2,152 @@ package database
 
 import (
 	"context"
-	"sort"
-	"sync"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 
 	orbitdb "berty.tech/go-orbit-db"
 	"berty.tech/go-orbit-db/accesscontroller"
 	"berty.tech/go-orbit-db/iface"
-	"berty.tech/go-orbit-db/stores"
-	"berty.tech/go-orbit-db/stores/documentstore"
 
 	// config "github.com/ipfs/go-ipfs-config"
+
 	icore "github.com/ipfs/interface-go-ipfs-core"
 	config "github.com/ipfs/kubo/config"
 	"github.com/ipfs/kubo/core"
 	"github.com/libp2p/go-libp2p/core/crypto"
-	"github.com/libp2p/go-libp2p/core/event"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/mitchellh/mapstructure"
-	"go.uber.org/zap"
-
-	"github.com/mrusme/superhighway84/cache"
-	"github.com/mrusme/superhighway84/models"
 )
 
-type Database struct {
-	ctx              context.Context
-	ConnectionString string
-	URI              string
-	CachePath        string
-	Cache            *cache.Cache
+const (
+	DEFAULT_PATH = "dafault"
+	PROGRAMSDB   = "self.programs"
+)
 
-	Logger *zap.Logger
+type Instance struct {
+	ctx context.Context
+
+	Dir  string //orbitdb dirctory
+	Repo string
 
 	IPFSNode    *core.IpfsNode
 	IPFSCoreAPI icore.CoreAPI
 
-	OrbitDB orbitdb.OrbitDB
-	Store   orbitdb.DocumentStore
-	Events  event.Subscription
+	OrbitDB  orbitdb.OrbitDB
+	Programs orbitdb.KeyValueStore
 }
 
-func (db *Database) init() error {
-	var err error
+// BootInstance 启动一个实例
+func BootInstance(ctx context.Context, repoPath, dbpath string) (ins *Instance, err error) {
 
-	ctx := context.Background()
-
-	db.Logger.Debug("initializing NewOrbitDB ...")
-	db.OrbitDB, err = orbitdb.NewOrbitDB(ctx, db.IPFSCoreAPI, &orbitdb.NewOrbitDBOptions{
-		Directory: &db.CachePath,
-		Logger:    db.Logger,
-	})
-	if err != nil {
-		return err
+	ins = new(Instance)
+	if repoPath == DEFAULT_PATH {
+		repoPath, err = config.PathRoot()
+		if err != nil {
+			return
+		}
 	}
 
+	if dbpath == DEFAULT_PATH {
+		dbpath, err = os.UserHomeDir()
+		if err != nil {
+			return
+		}
+
+		dbpath = filepath.Join(dbpath, "orbitdb")
+	}
+
+	ins.Dir = dbpath
+	ins.Repo = repoPath
+
+	if err = setupPlugins(repoPath); err != nil {
+		return
+	}
+
+	ins.IPFSNode, ins.IPFSCoreAPI, err = createNode(ctx, repoPath)
+	if err != nil {
+		return
+	}
+
+	ins.OrbitDB, err = orbitdb.NewOrbitDB(ctx, ins.IPFSCoreAPI, &orbitdb.NewOrbitDBOptions{
+		Directory: &ins.Dir,
+	})
+	if err != nil {
+		return
+	}
+
+	_, err = ins.GetProgramsDB()
+
+	return
+}
+
+func (ins *Instance) CreateDB(name, storetype string, accesseIDs []string) (db iface.Store, err error) {
+
+	if name == PROGRAMSDB {
+		err = fmt.Errorf("name can not be '%s'", PROGRAMSDB)
+		return
+	}
 	ac := &accesscontroller.CreateAccessControllerOptions{
 		Access: map[string][]string{
-			"write": {
-				"*",
-			},
+			"write": accesseIDs,
 		},
 	}
 
-	if err != nil {
-		return err
-	}
-
-	// addr, err := db.OrbitDB.DetermineAddress(db.ctx, db.Name, "docstore", &orbitdb.DetermineAddressOptions{})
-	// if err != nil {
-	//   return err
-	// }
-	// db.URI = addr.String()
-
-	storetype := "docstore"
-	db.Logger.Debug("initializing OrbitDB.Docs ...")
-	db.Store, err = db.OrbitDB.Docs(ctx, db.ConnectionString, &orbitdb.CreateDBOptions{
-		AccessController:  ac,
-		StoreType:         &storetype,
-		StoreSpecificOpts: documentstore.DefaultStoreOptsForMap("id"),
-		Timeout:           time.Second * 600,
+	db, err = ins.OrbitDB.Create(ins.ctx, name, storetype, &orbitdb.CreateDBOptions{
+		AccessController: ac,
 	})
 	if err != nil {
-		return err
+		return
 	}
 
-	db.Logger.Debug("subscribing to EventBus ...")
-	db.Events, err = db.Store.EventBus().Subscribe(new(stores.EventReady))
-	return err
+	dbinfo, err := json.Marshal(DBInfo{
+		Name:    name,
+		Type:    storetype,
+		Address: db.Address().String(),
+		AddedAt: time.Now().String(),
+	})
+	if err != nil {
+		return
+	}
+
+	_, err = ins.Programs.Put(ins.ctx, db.Address().String(), dbinfo)
+
+	return
 }
 
-func (db *Database) GetOwnID() string {
-	return db.OrbitDB.Identity().ID
+func (ins *Instance) GetDB(address string) (db iface.Store, err error) {
+
+	db, err = ins.OrbitDB.Open(ins.ctx, address, &orbitdb.CreateDBOptions{})
+	if err != nil {
+		return
+	}
+	err = db.Load(ins.ctx, -1)
+	return
 }
 
-func (db *Database) GetOwnPubKey() crypto.PubKey {
-	pubKey, err := db.OrbitDB.Identity().GetPublicKey()
+func (ins *Instance) AddDB(address string) (db iface.Store, err error) {
+
+	db, err = ins.GetDB(address)
+	if err != nil {
+		return
+	}
+	_, err = ins.Programs.Put(ins.ctx, address, []byte{})
+
+	return
+}
+
+func (ins *Instance) RemoveDB(address string) (err error) {
+	_, err = ins.Programs.Delete(ins.ctx, address)
+	return
+}
+
+func (ins *Instance) GetOwnID() string {
+	return ins.OrbitDB.Identity().ID
+}
+
+func (ins *Instance) GetOwnPubKey() crypto.PubKey {
+	pubKey, err := ins.OrbitDB.Identity().GetPublicKey()
 	if err != nil {
 		return nil
 	}
@@ -105,211 +155,28 @@ func (db *Database) GetOwnPubKey() crypto.PubKey {
 	return pubKey
 }
 
-func (db *Database) connectToPeers() error {
-	var wg sync.WaitGroup
-
-	peerInfos, err := config.DefaultBootstrapPeers()
-	if err != nil {
-		return err
-	}
-
-	wg.Add(len(peerInfos))
-	for _, peerInfo := range peerInfos {
-		go func(peerInfo *peer.AddrInfo) {
-			defer wg.Done()
-			err := db.IPFSCoreAPI.Swarm().Connect(db.ctx, *peerInfo)
-			if err != nil {
-				db.Logger.Error("failed to connect", zap.String("peerID", peerInfo.ID.String()), zap.Error(err))
-			} else {
-				db.Logger.Debug("connected!", zap.String("peerID", peerInfo.ID.String()))
-			}
-		}(&peerInfo)
-	}
-	wg.Wait()
-	return nil
+func (ins *Instance) Close() {
+	ins.OrbitDB.Close()
 }
 
-func NewDatabase(
-	ctx context.Context,
-	dbConnectionString string,
-	dbCache string,
-	cch *cache.Cache,
-	logger *zap.Logger,
-) (*Database, error) {
-	var err error
-
-	db := new(Database)
-	db.ctx = ctx
-	db.ConnectionString = dbConnectionString
-	db.CachePath = dbCache
-	db.Cache = cch
-	db.Logger = logger
-
-	db.Logger.Debug("getting config root path ...")
-	defaultPath, err := config.PathRoot()
-	if err != nil {
-		return nil, err
-	}
-
-	db.Logger.Debug("setting up plugins ...")
-	if err := setupPlugins(defaultPath); err != nil {
-		return nil, err
-	}
-
-	db.Logger.Debug("creating IPFS node ...")
-	db.IPFSNode, db.IPFSCoreAPI, err = createNode(ctx, defaultPath)
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
-}
-
-func (db *Database) Connect(onReady func(address string)) error {
-	var err error
-
-	db.Logger.Info("connecting to peers ...")
-	// go func() {
-	err = db.connectToPeers()
-	if err != nil {
-		db.Logger.Error("failed to connect: %s", zap.Error(err))
-	} else {
-		db.Logger.Debug("connected to peer!")
-	}
-	// }()
-
-	db.Logger.Info("initializing database connection ...")
-	// if db.Init {
-	err = db.init()
-	if err != nil {
-		db.Logger.Error("%s", zap.Error(err))
-		return err
-	}
-	// } else {
-	//   err = db.open()
-	//   if err != nil {
-	//     return err
-	//   }
-	// }
-
-	db.Logger.Info("running ...")
-
-	go func() {
-		for {
-			for ev := range db.Events.Out() {
-				db.Logger.Debug("got event", zap.Any("event", ev))
-				switch ev.(type) {
-				case stores.EventReady:
-					db.URI = db.Store.Address().String()
-					onReady(db.URI)
-					continue
-				}
-			}
+func (ins *Instance) GetProgramsDB() (program map[string][]byte, err error) {
+	if ins.Programs == nil && ins.OrbitDB != nil {
+		ins.Programs, err = ins.OrbitDB.KeyValue(ins.ctx, PROGRAMSDB, &orbitdb.CreateDBOptions{})
+		if err != nil {
+			return
 		}
-	}()
-
-	err = db.Store.Load(db.ctx, -1)
-	if err != nil {
-		db.Logger.Error("%s", zap.Error(err))
-		// TODO: clean up
-		return err
+		err = ins.Programs.Load(ins.ctx, -1)
+		if err != nil {
+			return
+		}
 	}
 
-	db.Logger.Debug("connect done")
-	return nil
+	return ins.Programs.All(), nil
 }
 
-func (db *Database) Disconnect() {
-	db.Events.Close()
-	db.Store.Close()
-	db.OrbitDB.Close()
-}
-
-func (db *Database) SubmitArticle(article *models.Article) error {
-	entity, err := structToMap(*article)
-	if err != nil {
-		return err
-	}
-	entity["type"] = "article"
-
-	_, err = db.Store.Put(db.ctx, entity)
-	return err
-}
-
-func (db *Database) GetArticleByID(id string) (models.Article, error) {
-	entity, err := db.Store.Get(db.ctx, id, &iface.DocumentStoreGetOptions{CaseInsensitive: false})
-	if err != nil {
-		return models.Article{}, err
-	}
-
-	var article models.Article
-	err = mapstructure.Decode(entity[0], &article)
-	if err != nil {
-		return models.Article{}, err
-	}
-
-	return article, nil
-}
-
-func (db *Database) ListArticles() ([]*models.Article, []*models.Article, error) {
-	var articles []*models.Article
-	var articlesMap = make(map[string]*models.Article)
-
-	_, err := db.Store.Query(db.ctx, func(e interface{}) (bool, error) {
-		entity := e.(map[string]interface{})
-		if entity["type"] == "article" {
-			var article models.Article
-			err := mapstructure.Decode(entity, &article)
-			if err == nil {
-				// TODO: Not sure why mapstructure won't convert this field and simply
-				//       leave it ""
-				if entity["in-reply-to-id"] != nil {
-					article.InReplyToID = entity["in-reply-to-id"].(string)
-				}
-				db.Cache.LoadArticle(&article)
-				articles = append(articles, &article)
-				articlesMap[article.ID] = articles[(len(articles) - 1)]
-			}
-			return true, err
-		}
-		return false, nil
-	})
-	if err != nil {
-		return articles, nil, err
-	}
-
-	sort.SliceStable(articles, func(i, j int) bool {
-		return articles[i].Date > articles[j].Date
-	})
-
-	var articlesRoots []*models.Article
-	for i := 0; i < len(articles); i++ {
-		if articles[i].InReplyToID != "" {
-			inReplyTo := articles[i].InReplyToID
-			if _, exist := articlesMap[inReplyTo]; exist {
-
-				(*articlesMap[inReplyTo]).Replies =
-					append((*articlesMap[inReplyTo]).Replies, articles[i])
-				(*articlesMap[inReplyTo]).LatestReply = articles[i].Date
-				continue
-			}
-		}
-		articlesRoots = append(articlesRoots, articles[i])
-	}
-
-	sort.SliceStable(articlesRoots, func(i, j int) bool {
-		iLatest := articlesRoots[i].LatestReply
-		if iLatest <= 0 {
-			iLatest = articlesRoots[i].Date
-		}
-
-		jLatest := articlesRoots[j].LatestReply
-		if jLatest <= 0 {
-			jLatest = articlesRoots[j].Date
-		}
-
-		return iLatest > jLatest
-	})
-
-	return articles, articlesRoots, nil
+type DBInfo struct {
+	Name    string `json:"name"`
+	Type    string `json:"type"`
+	Address string `json:"address"`
+	AddedAt string `json:"addat"`
 }
--- database/helpers.go
+++ database/helpers.go
@@ -3,18 +3,23 @@ package database
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	// files "github.com/ipfs/go-ipfs-files"
-	files "github.com/ipfs/go-libipfs/files"
+
 	icore "github.com/ipfs/interface-go-ipfs-core"
+	"github.com/ipfs/interface-go-ipfs-core/options"
+	config "github.com/ipfs/kubo/config"
 	"github.com/ipfs/kubo/core"
 	"github.com/ipfs/kubo/core/coreapi"
 	"github.com/ipfs/kubo/core/node/libp2p"
 	"github.com/ipfs/kubo/plugin/loader"
 	"github.com/ipfs/kubo/repo/fsrepo"
 	"github.com/mitchellh/mapstructure"
+	"go.uber.org/zap"
 )
 
 func setupPlugins(path string) error {
@@ -33,11 +38,38 @@ func setupPlugins(path string) error {
 
 	return nil
 }
+func createRepo(repoPath string) error {
 
+	var cfg *config.Config
+	var err error
+
+	identity, err := config.CreateIdentity(os.Stdout, []options.KeyGenerateOption{
+		options.Key.Type("ed25519"),
+	})
+	if err != nil {
+		return err
+	}
+	cfg, err = config.InitWithIdentity(identity)
+	if err != nil {
+		return err
+	}
+	cfg.Pubsub.Enabled = config.True
+
+	// Create the repo with the config
+	err = fsrepo.Init(repoPath, cfg)
+	if err != nil {
+		return fmt.Errorf("failed to init node: %s", err)
+	}
+
+	return nil
+}
 func createNode(ctx context.Context, repoPath string) (*core.IpfsNode, icore.CoreAPI, error) {
 	repo, err := fsrepo.Open(repoPath)
 	if err != nil {
-		return nil, nil, err
+		err = createRepo(repoPath)
+		if err != nil {
+			return nil, nil, err
+		}
 	}
 
 	nodeOptions := &core.BuildCfg{
@@ -62,20 +94,6 @@ func createNode(ctx context.Context, repoPath string) (*core.IpfsNode, icore.Cor
 	return node, coreAPI, nil
 }
 
-func getUnixfsNode(path string) (files.Node, error) {
-	st, err := os.Stat(path)
-	if err != nil {
-		return nil, err
-	}
-
-	f, err := files.NewSerialFile(path, false, st)
-	if err != nil {
-		return nil, err
-	}
-
-	return f, nil
-}
-
 func structToMap(v interface{}) (map[string]interface{}, error) {
 	vMap := &map[string]interface{}{}
 
@@ -85,4 +103,26 @@ func structToMap(v interface{}) (map[string]interface{}, error) {
 	}
 
 	return *vMap, nil
+}
+
+func newLogger(filename string) (*zap.Logger, error) {
+	if runtime.GOOS == "windows" {
+		zap.RegisterSink("winfile", func(u *url.URL) (zap.Sink, error) {
+			return os.OpenFile(u.Path[1:], os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
+		})
+	}
+
+	cfg := zap.NewDevelopmentConfig()
+	if runtime.GOOS == "windows" {
+		cfg.OutputPaths = []string{
+			"stdout",
+			"winfile:///" + filename,
+		}
+	} else {
+		cfg.OutputPaths = []string{
+			filename,
+		}
+	}
+
+	return cfg.Build()
 }
