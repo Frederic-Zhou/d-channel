0c556c5b233e8514ce1d2456c97402617d06cb2b
Frederic <zetachow@163.com>
2023-01-29 16:58:35 +0800

    调试

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"context"
+	"d-channel/database"
+	"log"
+)
+
 func main() {
+
+	ins, err := database.BootInstance(context.TODO(), database.DEFAULT_PATH, database.DEFAULT_PATH)
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	pk, err := ins.GetOwnPubKey()
+	log.Println(ins.GetOwnID(), pk, err)
+
+	peers, err := ins.IPFSCoreAPI.Swarm().Peers(context.TODO())
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	for i, v := range peers {
+		log.Println(i, v.Address())
+	}
+
+	ins.Close()
 
 }
