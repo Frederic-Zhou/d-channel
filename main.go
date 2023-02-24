160ce3a0f2a8f06a2617ab08f72d71a27f90999f
Frederic <zetachow@163.com>
2023-02-24 10:58:17 +0800

    增加命令行参数和环境变量参数

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "d-channel/httpapi"
+import (
+	"d-channel/httpapi"
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	//TODO: set port from command arguments
-	httpapi.Run(":8000")
+	// 定义命令行参数
+	port := flag.String("p", "", "The port to listen on.")
+
+	// 解析命令行参数
+	flag.Parse()
+
+	// 如果命令行参数不存在，则从环境变量中读取
+	if *port == "" {
+		*port = os.Getenv("DAPPPORT")
+	}
+
+	// 如果还是不存在，则使用默认值
+	if *port == "" {
+		*port = "8000"
+	}
+
+	// 输出结果
+	fmt.Printf("Listening on port %s...\n", *port)
+
+	// 程序继续执行...
+
+	httpapi.Run(fmt.Sprintf(":%s", *port))
 
 }
